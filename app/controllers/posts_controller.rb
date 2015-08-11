4332af332a767b17e0a4fce832a72cb481b6941a
dananagle <dananagle@Danas-MacBook-Air.local>
2015-08-11 13:23:21 -0500

    stole styling from surf and paddle lab 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,37 @@
 class PostsController < ApplicationController
-  def name
+
+  class Post
+    attr_reader :name, :photo
+    def initialize(name, photo)
+      @name = name
+      @photo = photo
+    end
   end
 
-  def photo
+  before_action do
+
+    @post_name = []
+    @post_name << Post.new("Hear me now," ,'IMG_1622.JPG')
+    @post_name << Post.new("And the knight with his banner" ,"IMG_1653.JPG")
+    @post_name << Post.new("all bravely unfurled" ,"IMG_1654.JPG")
+    @post_name << Post.new("Now Hurls down" ,"IMG_1659.JPG")
+    @post_name << Post.new("His Gauntlet, To Thee" ,"IMG_1661.JPG")
   end
+
+
+
+  def index
+    @post = @post_name.first
+    render :show
+  end
+
+  def show
+    @post = @post_name.find do |post|
+      post.name == params[:post_name]
+    end
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/name'
 
-  get 'posts/photo'
+
+  root 'posts#index'
+
+  get "posts/:post_name" => "posts#show", as: :post
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
